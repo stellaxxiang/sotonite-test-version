92da79209013695236cc1ca45abdd130b2d4f2ba
Xuan Xiang <stellaxxiang@gmail.ocm>
2017-05-08 01:07:50 +0100

    use web.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -42,6 +42,7 @@ module.exports = function(config) {
       'bower_components/js-rich-marker/src/richmarker.js',
       'bower_components/angular-google-maps/dist/angular-google-maps.js',
       'bower_components/angular-local-storage/dist/angular-local-storage.js',
+      'bower_components/ng-webworker/src/ng-webworker.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
