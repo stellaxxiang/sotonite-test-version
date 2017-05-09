de21b5219959336afc23ce4b06428fd9312952b3
Xuan Xiang <stellaxxiang@gmail.ocm>
2017-05-09 15:49:16 +0100

    web.js package.json

--- web.js
+++ web.js
@@ -5,4 +5,8 @@ var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
