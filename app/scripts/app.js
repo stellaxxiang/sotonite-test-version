bd103a40f63215b81dfad78846b36bcb51902ec5
Xuan Xiang <stellaxxiang@gmail.ocm>
2017-05-09 17:55:28 +0100

    change

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,40 +9,14 @@
  * Main module of the application.
  */
 angular
-  .module('odcw2App', ['ngAnimate', 'ngAria', 'uiGmapgoogle-maps', 'ngRoute', 'ui.router'])
+  .module('odcw2App', ['ngAnimate', 'ngAria', 'ngRoute'])
 	//'ngAnimate', 'ngAria', 'uiGmapgoogle-maps', 'ngRoute', 'ui.router'
-  .config(function(uiGmapGoogleMapApiProvider){
-	uiGmapGoogleMapApiProvider.configure({
-        key: 'AIzaSyDyX25B6rkfX1gLfmnY5eiFHqBepw66TB8',
-        v: '3.28',
-        //libraries: 'weather,geometry,visualization'
-    });
-})
   .config(function ($locationProvider) {
 	$locationProvider.html5Mode({
 		enabled: true,
 		requireBase: false}).hashPrefix('');
   })
-  .config(function ($stateProvider) {
-	$stateProvider
-	.state('home', {
-		url: '/',
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-	.state('atm', {
-		url: '/atm',
-        templateUrl: 'views/atmTemplate.html',
-        controller: 'atmController',
-		resolve: {
-			atmData: function(atmService) {
-				return atmService.getAtms().then(function(response){
-					return response;
-				});
-			}
-		}
-      });
-})
+
 .config(function ($urlRouterProvider) {
 	$urlRouterProvider.when('/', '/');
 	$urlRouterProvider.otherwise('/');
