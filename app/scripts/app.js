11e7059f82b044fe7256f3ecd3b6b4bb5029391d
Xuan Xiang <stellaxxiang@gmail.ocm>
2017-05-09 17:17:04 +0100

    change

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@
  * Main module of the application.
  */
 angular
-  .module('odcw2App', ['ngAnimate', 'ngAria', 'uiGmapgoogle-maps', 'ngRoute', 'ui.router'])
+  .module('odcw2App', ['ngAnimate', 'ngAria', 'ui.router', 'uiGmapgoogle-maps', 'ngRoute'])
   .config(function(uiGmapGoogleMapApiProvider){
 	uiGmapGoogleMapApiProvider.configure({
         key: 'AIzaSyDyX25B6rkfX1gLfmnY5eiFHqBepw66TB8',
