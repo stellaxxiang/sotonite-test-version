9177a1d9c7605177bedea459f942e7f4546eb35f
Xuan Xiang <stellaxxiang@gmail.ocm>
2017-05-10 02:52:22 +0100

    web.js

--- web.js
+++ web.js
@@ -5,21 +5,10 @@ var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-
-// serve angular front end files from root path
-//app.Router().use('/', express.static('dist', { redirect: false }));
-//app.use('/scripts', express.static(__dirname + '/dist/scripts'));
-//app.use('/fonts', express.static(__dirname + '/dist/fonts'));
-//app.use('/styles', express.static(__dirname + '/dist/styles'));
-//app.use('/images', express.static(__dirname + '/dist/images'));
-//
-//app.all('/*', function(req, res) {
-//    // Just send the index.html for other files to support HTML5Mode
-//    res.sendFile('/dist/index.html', { root: __dirname });
-//});
-
-app.set('port', process.env.PORT || 5000);
-
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(process.env.PORT || 5000);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
+app.get('/atm', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
 });
